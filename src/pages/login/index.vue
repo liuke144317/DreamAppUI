ef9fb42288b30598f626597800ed78f4537775de
unknown <1443174787@qq.com>
2021-07-05 23:43:56 +0800

    完成登录

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -7,7 +7,7 @@
 					<input
 					class="input"
 					type="text" 
-					value="username" 
+					v-model="username" 
 					placeholder="用户名"
 					placeholder-style="color:#FFFFFF; text-align: center;"
 					/>
@@ -17,14 +17,14 @@
 					class="input"
 					type="text" 
 					password="true"
-					value="password"
+					v-model="password"
 					placeholder="密码"
 					placeholder-style="color:#FFFFFF; text-align: center;"
 					/>
 				</view>
 			</view>
-			<view class="sign">
-				<view class="sign-title" @tap="toLogin">
+			<view class="sign"  @tap="toLogin">
+				<view class="sign-title">
 					登录
 				</view>
 				<text class="sign-icon text-white cuIcon-right"></text>
@@ -50,16 +50,28 @@
 			@tap="showDrawer"
 			></text>
 		</view>
+		<!-- popup -->
+		<uni-popup ref="popup" type="center" class="popup" duration="2000">
+		    <view class="dialog">
+		    	<text>{{publishStatus}}</text>
+		    </view>
+		</uni-popup>
 	</view>
 </template>
 
 <script lang="ts">
 	import { Vue, Component } from 'vue-property-decorator'
-	@Component({})
+	import uniPopup from '@/components/uni-popup/uni-popup.vue'
+	@Component({
+		components: {
+			uniPopup
+		}
+	})
 	export default class LoginIndex extends Vue {
 		ifDrawerShow: boolean = false;
 		username: string = '';
 		password: string = '';
+		publishStatus: string = ''
 		showDrawer ():void {
 			this.ifDrawerShow = ! this.ifDrawerShow;
 		}
@@ -70,12 +82,39 @@
 		}
 		async toLogin ():Promise<void>  {
 			var res = await this.$store.dispatch('login/login', {username: this.username, password: this.password})
-			console.log('res', res)
+			if (res.statusCode === 200 && res.data.length !== 0) {//登录成功
+				this.publishStatus = '登录成功'
+				this.$refs.popup.open()
+				setTimeout(() => {
+					this.$refs.popup.close()
+					uni.redirectTo({
+						url: '/pages/index/index',
+					});
+				},1000)
+			} else {
+				this.publishStatus = '账号或密码错误！'
+				this.$refs.popup.open()
+				setTimeout(() => {
+					this.$refs.popup.close()
+				},1000)
+			}
 		}
 	}
 </script>
 
 <style>
 	@import url("./index.css");
+	.popup >>> .dialog{
+		width: 150px;
+		height: 100px;
+		text-align: center;
+		line-height: 100px;
+		border-radius: 5px;
+		background: rgba(0,0,0,.5);
+		color: #fff;
+	}
+	.popup >>> .uni-transition{
+		background: rgba(0,0,0,0)!important;
+	}
 </style>
 
